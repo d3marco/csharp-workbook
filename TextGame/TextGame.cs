d8271b9287dcc7b28b64eb2e2553a3a1f71b1782
DeMarco <demarcospears22@gmail.com>
2018-10-07 22:18:07 -0500

    textgame file added

---
+++ TextGame/TextGame.cs
@@ -0,0 +1,18 @@
+musing System;
+using threading;
+
+namespace TextGame
+{
+
+    class Program
+    {
+
+        public void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the cavern of secrets!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        }
+    }
+}
