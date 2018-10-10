2e1cf1d48176b71a68260763db6dc084f3d3895f
DeMarco <demarcospears22@gmail.com>
2018-10-10 17:31:26 -0500

    1st draft piglatin

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,7 +7,9 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
-
+            String userInput = "";
+            Console.WriteLine("Enter a word:")
+            Console.ReadLine();
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
@@ -15,6 +17,7 @@ namespace PigLatin
         public static string TranslateWord(string word)
         {
             // your code goes here
+            userInput = Console.ReadLine();
             return word;
         }
     }
