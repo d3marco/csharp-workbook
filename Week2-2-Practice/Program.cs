2feaa9e102ae5a59afef09124d7aadb48d7a4848
DeMarco <demarcospears22@gmail.com>
2018-10-10 19:23:07 -0500

    week2-2 practice

---
+++ Week2-2-Practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week2_2_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            userInput = Console.ReadLine(Convert.ToInt32());
+            Console.WriteLine("Please provide a number 0 - 100:");
+            if(userInput => 90)
+            {
+                Console.WriteLine("You got an A!");
+            }
+            else
+            {
+                Console.WriteLine("sorry buddy!")
+            }
+        }
+    }
+}
