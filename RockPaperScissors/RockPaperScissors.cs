be448d5d9643cfb4d0175d177cbf49147246bfc0
DeMarco <demarcospears22@gmail.com>
2018-10-15 18:36:20 -0500

    Final paperrockscissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -14,11 +14,45 @@ namespace RockPaperScissors
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
+        
         }
         
         public static string CompareHands(string hand1, string hand2)
         {
             // Your code here
+            if (hand1 == "rock" && hand2 == "rock")
+            {
+                Console.WriteLine("Tie");
+            } else if (hand1 == "rock" && hand2 == "scissors")
+            {
+                Console.WriteLine("hand1 wins!");
+            } else if (hand1 == "rock" && hand2 == "paper")
+            {
+                Console.WriteLine("hand2 wins!");
+            } else if (hand1 == "scissors" && hand2 == "rock")
+            {
+                Console.WriteLine("hand2 wins");
+            } else if (hand1 == "scissors" && hand2 == "paper")
+            {
+                Console.WriteLine("hand1 wins");
+            } else if (hand1 == "scissors" && hand2 == "scissors")
+             {
+                 Console.WriteLine("Tie");
+             } else if (hand1 == "paper" && hand2 == "paper")
+             {
+                 Console.WriteLine("Tie");
+             } else if (hand1 == "paper" && hand2 == "scissors")
+             {
+                 Console.WriteLine("hand2 wins");
+             } else if (hand1 == "paper" && hand2 == "rock")
+             {
+                 Console.WriteLine("hand1 wins");
+             } else
+             {
+                 Console.WriteLine("try again");
+             }
+        
+            
             return hand1 + ' ' + hand2;
         }
     }
