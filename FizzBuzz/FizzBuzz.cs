bca0698332fd8c755776fb709bc2a6896f0eb384
DeMarco <demarcospears22@gmail.com>
2018-10-22 22:14:42 -0500

    updated

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -8,7 +8,7 @@ namespace FizzBuzz
         {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0 & i % 5 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
