52d74a3ced994295c494134dad4a5756be555c7a
DeMarco <demarcospears22@gmail.com>
2018-11-03 12:37:09 -0500

    first commit

---
+++ Gradebook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What's the name of the student?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the the students grades seprated by a comma or 'ok' to move to the next student");
+            int grade = Console.ReadLine();
+        }
+        public void Mean()
+        {
+            
+        }
+    }
+}
--- week4-practice/Program.cs
+++ week4-practice/Program.cs
@@ -1,91 +1,47 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
 
-namespace RockPaperScissors2
+namespace Week4practice
 {
     class Program
     {
-        public static void Main()
+        static void Main()
         {
-            try {
+            list<string> personName = new list<string>("John", "Mike", "Sam");
 
+            personName.Add("John");
+            personName.Add("Mike");
+            personName.Add("Sam");
+
+            foreach (String name in personName)
+            {
+                Console.WriteLine(personName);
             }
-            Console.WriteLine("Enter hand 1:");
-            string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
-            Console.WriteLine(CompareHands(hand1, hand2));
+            names.RemoveAt(2);
 
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
-            
-        
+            foreach (String name in personName)
+            {
+                Console.WriteLine(personName);
+            }
+
         }
-        /// implement exception
-        /// catch exception
-        // if user enter incorrect imput
-        
-        public static string CompareHands(string hand1, string hand2)
-        {
-            // Your code here
-            if (hand1 == "rock" && hand2 == "rock")
-            {
-                Console.WriteLine("Tie");
-            } else if (hand1 == "rock" && hand2 == "scissors")
-            {
-                Console.WriteLine("hand1 wins!");
-            } else if (hand1 == "rock" && hand2 == "paper")
-            {
-                Console.WriteLine("hand2 wins!");
-            } else if (hand1 == "scissors" && hand2 == "rock")
-            {
-                Console.WriteLine("hand2 wins");
-            } else if (hand1 == "scissors" && hand2 == "paper")
-            {
-                Console.WriteLine("hand1 wins");
-            } else if (hand1 == "scissors" && hand2 == "scissors")
-             {
-                 Console.WriteLine("Tie");
-             } else if (hand1 == "paper" && hand2 == "paper")
-             {
-                 Console.WriteLine("Tie");
-             } else if (hand1 == "paper" && hand2 == "scissors")
-             {
-                 Console.WriteLine("hand2 wins");
-             } else if (hand1 == "paper" && hand2 == "rock")
-             {
-                 Console.WriteLine("hand1 wins");
-             } else
-             {
-                 Console.WriteLine("try again");
-             }
-        
-            
-            return hand1 + ' ' + hand2;
-        }
-        // public static bool test()
-        // {
-        //     return 0;
-
-        //     CompareHands("Paper", "Paper") == 0 &&
-        //     CompareHands("Paper", "Rock") == 1 &&
-        //     CompareHands("Paper", "Scissors") == 2 &&
-        //     CompareHands("Rock", "Paper") == 2 &&
-        //     CompareHands("Rock", "Rock") == 0 &&
-        //     CompareHands("Rock", "Scissors") == 1 &&
-        //     CompareHands("Scissors", "Paper") == 1 &&
-        //     CompareHands("Scissors", "Scissors") == 0 &&
-        //     CompareHands("Scissors", "Rock") == 2;
-            
-        // }
-//     }
-// }
-
-
-// try{
-//     double answer1 = Divide(17.0, 1.0);
-//     Console.WriteLine(answer1);
-// } catch (Exception e) {
-//     Console.WriteLine("There was a error with the function");
-// } finally {
-//     Console.WriteLine ("Thanks for playing");
+    }
 }
+
+//peek();
+//pop();
+//push(); push to stack
+//count();
+//Deepth first before breath first
+
+//Stack<String> var*stackname = new Stack<String>();
+// var.Push("John");
+// var.Push("Sam");
+// var.Push("Paul");
+
+// while(name.Count > 0)
+// {
+//     String currentName = names.Pop();
+//     Console.WriteLine("Processing" +currentName);
+// }
