0c36ca03f4e505c6078bd3e182d080095a070536
DeMarco <demarcospears22@gmail.com>
2018-11-09 20:09:49 -0600

    Treehouse Defender game

---
+++ CarGarage/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Car blueCar = new Car("blue");
+        Garage smallGarage = new Garage(2);
+
+        smallGarage.ParkCar(blueCar, 0);
+        Console.WriteLine(smallGarage.Cars);
+    }
+}
+
+class Car
+{
+    public Car(string initialColor)
+    {
+        Color = initialColor;
+    }
+
+    public string Color { get; private set; }
+}
+
+class Garage
+{
+    private Car[] cars;
+
+    public Garage(int initialSize)
+    {
+        Size = initialSize;
+        this.cars = new Car[initialSize];
+    }
+
+    public int Size { get; private set; }
+
+    public void ParkCar(Car car, int spot)
+    {
+        cars[spot] = car;
+    }
+
+    public string Cars
+    {
+        get
+        {
+            for (int i = 0; i < cars.Length; i++)
+            {
+                if (cars[i] != null)
+                {
+                    Console.WriteLine(String.Format("The {0} car is in spot {1}.", cars[i].Color, i));
+                }
+            }
+            return "That's all!";
+        }
+    }
+    class Person
+    {
+        
+    }
+}
+
---
+++ Treehouse/Defender/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Defender
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map(8, 5);
+
+            Point point = new Point(4,2);
+
+            Console.WriteLine(point.DistanceTo(5,5));
+            
+        }
+    }
+
+    class Tower
+    {
+
+    }
+    class Invader
+    {
+
+    }
+    class Map
+    {
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public bool OnMap(Point point)
+        {
+            return point.X >= 0 &&
+             point.X <= 8 && point.Y >= 0
+             && point.Y <= 5;
+
+        }
+    }
+    class Path
+    {
+
+    }
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int DistanceTo(int x, int y)
+        {
+        //    int xDiff = X - x;
+        //    int yDiff = Y - y; 
+
+        //    int xDiffSquared = xDiff * xDiff;
+        //    int yDiffSquared = yDiff * yDiff;
+
+        //   return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
+          return (int)Math.Sqrt(Math.Pow(X-x,2) + Math.Pow(Y-y,2));
+
+
+
+        }
+    }
+}
