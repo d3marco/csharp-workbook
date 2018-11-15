1dab2560f67190c5d0dec673807090f769c01792
DeMarco <demarcospears22@gmail.com>
2018-11-14 19:42:32 -0600

    first update

---
+++ Interface/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interface
+{
+    public interface ITaxable
+    {
+        double taxRate();
+    }
+
+
+    public class Shoes : ITaxable
+    {
+
+        public double AfterTax;
+        public Shoes(double totalPrice)
+        {
+            this.hourlyRate = hourly;
+        }
+    }
+
+}
+public class Room : IRentable
+{
+
+    {
+        public double dailyRate;
+    public Romm(double daily)
+    {
+        this.dailyRate = daily;
+    }
+
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Boat b1 = new Boat(40);
+        Boat b2 = new Boat(100);
+
+        Room r1 = new Room(145);
+        Room r2 = new Room(300);
+    }
+}
+    
+}
+'[
+    5'
