9e354d7a5546d34870a6fa6bae9e5bf7f0ac3b44
d3marco31 <demarcospears22@gmail.com>
2018-11-26 18:41:41 -0600

    2nd draft Hanoi

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -6,55 +6,61 @@ namespace TowersOfHanoi
 {
     class Program
     {
-      Game game = new Game();  
+        public void Main()
+        {
+            Game game = new Game();
+            CheckForWin();
+        }
+        
     }
     class Block
     {
-        int Weight {public get; private set; }
+        int weight { public get; private set; }
     }
     class Tower
     {
         Stack<int> blocks = new Stack<int>();
-        
+
     }
     class Game
     {
-        Dictionary<int,string> towers = new Dictionary<int, string>();
+        // Console.WriteLine("Which one?");
+        Dictionary<int, string> towers = new Dictionary<int, string>();
 
         public Game()
         {
-        towers.Add("A", new Tower());
-        towers.Add("B", new Tower());
-        towers.Add("C", new Tower());
-        
-        Block block1 = new Block();
-        Block block2 = new Block();
-        Block block3 = new Block();
-        block1.Weight = 1;
-        block2.Weight = 10;
-        block3.Weight = 100;
+            towers.Add("A", new Tower());
+            towers.Add("B", new Tower());
+            towers.Add("C", new Tower());
 
-        towers["A"].blocks.Push(block3);
-        towers["A"].blocks.Push(block2);
-        towers["A"].blocks.Push(block1);
+            Block block1 = new Block();
+            Block block2 = new Block();
+            Block block3 = new Block();
+            block1.weight = 1;
+            block2.weight = 10;
+            block3.weight = 100;
+
+            towers["A"].blocks.Push(block3);
+            towers["A"].blocks.Push(block2);
+            towers["A"].blocks.Push(block1);
 
         }
-        public print()
+        public string print()
         {
-           foreach( var towerKey in towers.Keys) 
-           {
-               foreach(Block block in towers[towerKey].blocks)
-               {
-                   Console.WriteLine(" Tower :" + towerKey + " Weight: " + block.weight );
-               }
-           }
+            foreach (var towerKey in towers.Keys)
+            {
+                foreach (Block block in towers[towerKey].blocks)
+                {
+                    Console.WriteLine(" Tower :" + towerKey + " Weight: " + block.weight);
+                }
+            }
         }
-        public MovePiece(string popOff, string pushOn)
+        public string MovePiece(string popOff, string pushOn)
         {
             Block block = towers[popOff].blocks.Pop();
             towers[pushOn].blocks.Push(block);
             print();
-            
+
         }
         public bool isLegal(string popOff, string pushOn)
         {
@@ -62,20 +68,37 @@ namespace TowersOfHanoi
             {
                 return false;
             }
-             if (towers[pushOn].blocks.Count == 0)
+            if (towers[pushOn].blocks.Count == 0)
             {
                 return true;
             }
             Block popOffBlock = towers[popOff].blocks.Peek();
             Block pushOnBlock = towers[pushOn].blocks.Peek();
 
-            if (popOffBlock.Weight > pushOnBlock.Weight)
+            if (popOffBlock.weight > pushOnBlock.weight)
             {
                 return false;
 
-            }else{
+            }
+            else
+            {
                 return true;
             }
         }
+        public bool CheckForWin()
+        {
+            if (towers["B"].Count() || towers["C"].Count() == 4)
+            {
+                Console.WriteLine("You Won!");
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
+
 }
