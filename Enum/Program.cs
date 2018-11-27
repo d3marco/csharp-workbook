850d95aaed0c2f31b33e951a86ce96604bdb6225
d3marco31 <demarcospears22@gmail.com>
2018-11-26 20:03:41 -0600

    first enum

---
+++ Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enum
+{
+    enum PrimaryColors
+    {
+        RED, BLUE, YELLOW
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Enum!");
+            string primaryCombined = combinePrimaryColors(PrimaryColors.RED, PrimaryColors.YELLOW);
+            Console.WriteLine("Red + Yellow = "+primaryCombined);
+        }
+        public static String combinePrimaryColors(PrimaryColors color1, PrimaryColors color2)
+        {
+            
+        }
+    }
+}
