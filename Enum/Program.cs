1fe3d16d82b0d36cdc66bbd2924b4a2e2077e833
d3marco31 <demarcospears22@gmail.com>
2018-12-09 21:29:34 -0600

    Enum assignment

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,26 +1,22 @@
-﻿using System;
-
-namespace Enum
+﻿
+using System;
+					
+public class Program
 {
-    enum WeekDays
-    {
-        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("When is your birthday?");
-            string birthDay = Console.ReadLine();
-            // Console.WriteLine("Red + Yellow = "+primaryCombined);
-        }
-        public static string Age(this HtmlHelper helper, DateTime birthday)
-        {
-            DateTime now = DateTime.Today;
-            int age = now.Year - birthday.Year;
-            if (now < birthday.AddYears(age)) age--;
-
-            return age.ToString();
-        }
-    }
-}
+	public static void Main()   
+	{		
+        int myDay = 15;
+        int myMonth = 10;
+                
+		Console.WriteLine("Please input a year");
+		int myYear = Convert.ToInt32(Console.ReadLine());
+         DateTime dt = new DateTime(myYear,myMonth, myDay);
+         int dow = (int)dt.DayOfWeek;
+         var dowName = (DaysOfTheWeekWithNumbers)dow;
+        Console.WriteLine(dowName);
+ 	}	
+	enum DaysOfTheWeekWithNumbers
+	{
+		Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 0
+	}
+} 
