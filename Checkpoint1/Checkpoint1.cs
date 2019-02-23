a2a3688073d8c1b2352c7243e17ae9b7d3481deb
d3marco <demarcospears22@gmail.com>
2019-02-22 19:24:18 -0600

    Checkpoint1 refactored

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Checkpoint1
 {
@@ -6,7 +7,128 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FirstStep.Counter();
+            SecondStep.Ask();
+            Thread.Sleep(2000);
+            ThirdStep.Factorial();
+            Thread.Sleep(2000);
+            FourthStep.Chance();
+            Console.ReadLine();
+
         }
     }
+
+    class FirstStep
+    {
+        public static void Counter()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if ( i % 3 == 0)
+            {
+                    Console.WriteLine(i);
+            }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class SecondStep
+    {
+        public static void Ask()
+        {
+            int totalInput = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number or 'ok' to exit");
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "ok")
+                {
+                    break;
+
+                }
+                else
+                {
+                    totalInput += Convert.ToInt32(userInput);
+                    Console.WriteLine(totalInput);
+                } 
+                
+            }
+
+        }
+    }
+
+    class ThirdStep
+    {
+        public static void Factorial()
+        {
+            Console.WriteLine("Please enter your number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine(input + "! =" + fact);
+        }
+    }
+
+    class FourthStep
+    {
+        public static void Chance()
+        {
+            Random rand = new Random();
+            int winNumber = rand.Next(1, 11);
+            int guess;
+            int numOfGuess = 0;
+            bool winner = false;
+            int maxGuess = 4;
+
+
+            while (winner == false)
+            {
+
+                if (numOfGuess > maxGuess - 1)
+                {
+                    Console.WriteLine("You have reached the max number of guesses: " + maxGuess);
+                    break;
+                }
+
+                Console.WriteLine("Enter a number:");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+
+                if (guess > winNumber)
+                {
+                    numOfGuess++;
+                    Console.WriteLine("Guess is too high, try again. ");
+                    Console.WriteLine("The number of guesses is " + numOfGuess);
+                }
+                else if (guess < winNumber)
+                {
+                    numOfGuess++;
+                    Console.WriteLine("Guess is too low, try again. ");
+                    Console.WriteLine("The number of guesses is " + numOfGuess);
+                }
+                else if (guess == winNumber)
+                {
+                    numOfGuess++;
+                    Console.WriteLine("You win! Good job. ");
+                    Console.WriteLine("The number of guesses is " + numOfGuess);
+                    winner = true;
+                }
+
+            }
+        }
+
+        class FifthStep
+        {
+            public static void 
+                4342 5801 06546633 0522 936
+        }
+
+    }
+
 }
---
+++ Youtube/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Youtube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "John";
+            int age = 35;
+
+            Console.WriteLine("There once was a man name " + userName);
+            Console.WriteLine("He was " + age + " years old");
+            Console.WriteLine("He really liked the name " + userName);
+            Console.WriteLine("But didn't like being " + age);
+
+            Console.ReadLine();
+        }
+    }
+}
